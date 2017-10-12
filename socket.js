5555b56e2cab21c43f947b5fea6dde631063307d
Kevin Kennis <kevinvkennis@gmail.com>
2017-10-11 22:11:59 -0700

    safety checks

--- socket.js
+++ socket.js
@@ -1,29 +1,56 @@
 const CryptoCompare = require('./services/cryptocompare');
+const R = require('ramda');
 
-function setupSocket(io) {
-    io.on('connection', setupSocket);
-}
+const setupSocket = (io) => { io.on('connection', setupConnection); };
 
 function setupConnection(socket) {
     const cc = new CryptoCompare();
     const subscriptions = {};
 
     socket.on('addCoin', (coin) => {
+        const isSafe = checkSafety(coin);
+
+        if (!isSafe) {
+            console.log('Bad Request');
+            return;
+        }
+
         cc.addCoin(coin);
         doSubscribe(coin);
     });
 
     socket.on('addCoins', (coins) => {
+        const isSafe = checkBatchSafety(coin);
+
+        if (!isSafe) {
+            console.log('Bad Request');
+            return;
+        }
+
         cc.addCoins(coin);
         coins.forEach(doSubscribe);
     });
 
     socket.on('removeCoin', (coin) => {
+        const isSafe = checkSafety(coin);
+
+        if (!isSafe) {
+            console.log('Bad Request');
+            return;
+        }
+
         cc.removeCoin(coin);
         doUnsubscribe(coin);
     });
 
     socket.on('removeCoins', (coins) => {
+        const isSafe = checkBatchSafety(coin);
+
+        if (!isSafe) {
+            console.log('Bad Request');
+            return;
+        }
+
         cc.removeCoins(coins);
         coins.forEach(doUnsubscribe);
     });
@@ -42,6 +69,25 @@ function setupConnection(socket) {
     function errorHandler(err) {
         console.log('Server Error:', err);
         socket.emit('error', err);
+    }
+
+    function checkSafety(coin) {
+        // For safety, make sure it is a string and length less than 10 (all tickers should be shorter than 10)
+        if (typeof coin !== 'string' || coin.length > 10) {
+            socket.emit('badrequest', { message: 'please provide a coin ticker symbol of type string' });
+            return false;
+        }
+
+        return true;
+    }
+
+    function checkBatchSafety(coins) {
+        if (!Array.isArray(coins)) {
+            socket.emit('badrequest', { message: 'please provide an array of coin strings' });
+            return false;
+        }
+
+        return R.every(checkSafety, coins);
     }
 }
 
